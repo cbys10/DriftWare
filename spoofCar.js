db1a6703b69cee58272bac9fdc8ddc5a78e24b1b
cbys10 <cbysket@gmail.com>
2023-11-02 18:36:08 -0400

    Create spoofCar.js

---
+++ spoofCar.js
@@ -0,0 +1,1 @@
+const storedData=JSON.parse(localStorage.getItem("mjs-drift-boss-game-v1.0.1-dailyreward"))||{};console.log(storedData),storedData.currentCar=prompt("what car do you want?"),localStorage.setItem("mjs-drift-boss-game-v1.0.1-dailyreward",JSON.stringify(storedData)),console.log(storedData),window.location.reload();
