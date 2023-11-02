7835f9f4298a10f92dd4885d85f6b7a9a60877e0
cbys10 <cbysket@gmail.com>
2023-11-02 18:39:22 -0400

    Create setCoins.js

---
+++ setCoins.js
@@ -0,0 +1,1 @@
+const storedData=JSON.parse(localStorage.getItem("mjs-drift-boss-game-v1.0.1-dailyreward"))||{};console.log(storedData),storedData.collectedCoin=prompt("How much coins do you want?"),localStorage.setItem("mjs-drift-boss-game-v1.0.1-dailyreward",JSON.stringify(storedData)),console.log(storedData),window.location.reload();
